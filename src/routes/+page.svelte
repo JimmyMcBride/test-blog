687d14a9d5d999aca9c8f43e8664dd11c5aba57b
Jimmy McBride <mcbride967@gmail.com>
2024-10-04 22:08:36 -0500

    removed discord auth to just blog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,2 @@
-<script lang="ts">
-	import { userStore } from "$lib/authStore"
-</script>
-
-<h1>
-	Welcome to My SvelteKit Blog{#if $userStore}, {$userStore.username}{/if}!
-</h1>
+<h1>Welcome to My SvelteKit Blog!</h1>
 <a class="text-blue-500" href="/blog">View my blogs!</a>
